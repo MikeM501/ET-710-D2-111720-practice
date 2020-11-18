58d774343dfce43f45ab93bdf151d56e7591e8ae
MikeM501 <marcos.castillo91@student.qcc.cuny.edu>
2020-11-18 00:05:49 -0500

    Data page complete

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable()
 export class CartService {
   items = [];
@@ -16,6 +18,13 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-constructor() { }
+   
+    getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+
+constructor( 
+private http: HttpClient) 
+{ }
 
 }
